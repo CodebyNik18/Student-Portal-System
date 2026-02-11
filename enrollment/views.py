7c809c3665f76be3813fae2bc3644d73de96f8ff
Nikhil <1837nik@gmail.com>
2026-02-11 22:21:45 +0530

    Fix in displayin pending assignment

--- enrollment/views.py
+++ enrollment/views.py
@@ -5,11 +5,11 @@ from assignments.models import SubmitAssignment, Assignment
 
 def dashboard(request):
     user = request.user
-    course = Enrollment.objects.get(user=user).get_course_display()
-    assignments = Assignment.objects.all()
+    course = Enrollment.objects.get(user=user)
+    assignments = Assignment.objects.filter(course_name=course.course)
     context = {
         'user': user.get_full_name().capitalize(),
-        'course': course,
+        'course': course.get_course_display(),
         'assignments': assignments
     }
     return render(request=request, template_name='student_dashboard.html', context=context)
